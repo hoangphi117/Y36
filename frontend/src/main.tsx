ab78c4a06eae4c3ff957d644d00abc9a7d2f574d
23120320 <23120320@student.hcmus.edu.vn>
2025-12-29 15:36:12 +0700

    frontend: config theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "./components/theme-provider";
+import router from "./routes/router";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </StrictMode>
+);
