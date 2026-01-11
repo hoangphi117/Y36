ab29a879aa520a4904e801bb9d10a6b65e8161df
23120341 <23120341@student.hcmus.edu.com>
2026-01-11 20:50:17 +0700

    feat: add API key middleware for secure access control

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,11 +4,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 const router = require('./routes/index');
 const cors = require('cors');
+const checkApiKey = require('./middlewares/apiKeyMiddleware');
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', router);
+app.use('/api',checkApiKey, router);
 
 app.get("/", (req, res) => {
   res.send("Hello mấy cưng =))");
