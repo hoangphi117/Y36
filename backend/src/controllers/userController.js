c2ba4708962d897eb4c6b04db3705f503962d20e
qcd <qn25397@gmail.com>
2026-01-14 20:45:09 +0700

    feat: refactor userController to improve code structure and add changePassword functionality

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,34 +1,33 @@
-const User = require('../models/User');
-
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 const updateMe = async (req, res) => {
   try {
-      const userId = req.user.id;
-      const { username, avatar_url, dark_mode } = req.body;
+    const userId = req.user.id;
+    const { username, avatar_url, dark_mode } = req.body;
 
-      const [updatedUser] = await User.updateById(userId, {
-        username,
-        avatar_url,
-        dark_mode,
+    const [updatedUser] = await User.updateById(userId, {
+      username,
+      avatar_url,
+      dark_mode,
+    });
+
+    if (!updatedUser) {
+      return res.status(404).json({
+        message: "User not found",
       });
-
-      if (!updatedUser) {
-        return res.status(404).json({
-          message: "User not found",
-        });
-      }
-
-      return res.status(200).json({
-        message: "User updated successfully",
-        user: updatedUser,
-      });
-    } catch (error) {
-      return res.status(500).json({ message: error.message });
     }
 
+    return res.status(200).json({
+      message: "User updated successfully",
+      user: updatedUser,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
-const Profile = async (req, res) => {
+const profile = async (req, res) => {
   try {
     const userId = req.user.id;
     const user = await User.findById(userId);
@@ -37,12 +36,43 @@ const Profile = async (req, res) => {
     }
 
     return res.status(200).json({ data: user });
-
   } catch (error) {
     return res.status(500).json({
       message: "Server error",
       error: error.message,
     });
   }
-}
-module.exports = { updateMe, Profile };
+};
+
+const changePassword = async (req, res) => {
+  try {
+    const userID = req.user.id;
+
+    const { currentPassword, newPassword } = req.body;
+
+    const user = await User.findById(userID);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isMatch = await User.comparePassword(
+      currentPassword,
+      user.password_hash
+    );
+    if (!isMatch) {
+      return res.status(400).json({ message: "Current password is incorrect" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(newPassword, salt);
+    await User.updateById(userID, { password_hash: hashedPassword });
+
+    return res.status(200).json({ message: "Password changed successfully" });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Server error",
+      error: error.message,
+    });
+  }
+};
+module.exports = { updateMe, profile,changePassword };
