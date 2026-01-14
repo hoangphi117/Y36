669c08966cfa6a3be744367f81d807473b8a50b9
23120272 <lehuy1519@gmail.com>
2026-01-14 23:59:18 +0700

    feat: create overlay when change game status memory game

---
+++ frontend/src/components/games/memory/GameBoardOverlay.tsx
@@ -0,0 +1,44 @@
+import { RoundButton } from "@/components/ui/round-button";
+import { motion } from "framer-motion";
+import { ChevronRight, RefreshCcw } from "lucide-react";
+
+interface GameStatusOverlayProps {
+    totalScore: number;
+    gameStatus?: "completed" | "lost" | "playing";
+    action: () => void;
+    currentLevel?: number;
+}
+
+const GameStatusOverlay = ({ totalScore, gameStatus, action, currentLevel }: GameStatusOverlayProps) => {
+    return (
+        <motion.div
+            className="bg-card rounded-2xl p-6 sm:p-12 text-center shadow-lg border-2 border-primary/20 mb-4 sm:mb-8"
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+        >
+            {gameStatus === "completed" && (
+                <>
+                    {currentLevel !== undefined && currentLevel < 5 && (
+                        <p className="text-xl sm:text-3xl font-black text-primary mb-2">Level {currentLevel + 1} Hoàn thành! 🎉</p>
+                    )}
+                    {currentLevel === 5 && (
+                        <p className="text-xl sm:text-3xl font-black text-primary mb-2">Chúc mừng! Bạn đã hoàn thành tất cả 6 cấp độ! 🏆</p>
+                    )}
+                </>
+            )}
+
+            {gameStatus === "lost" && (
+                <p className="text-xl sm:text-3xl font-black text-destructive mb-2">Thất bại 😞</p>
+            )}
+            <p className="text-foreground text-xl">Tổng điểm: <span className="font-bold text-primary text-xl">{totalScore}</span></p>
+
+            <RoundButton size="small" variant="primary" className="mt-3 rounded-sm" onClick={action} >
+                {gameStatus === "completed" ? "Màn tiếp theo" : "Chơi lại"}
+                {gameStatus === "completed" ? <ChevronRight className="w-4 h-4" /> : <RefreshCcw className="w-4 h-4" />}
+            </RoundButton>
+
+        </motion.div>
+    )
+}
+
+export {GameStatusOverlay};
