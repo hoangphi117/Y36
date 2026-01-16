994f7c00935d194023135149d8ab5d89e400eefa
qcd <qn25397@gmail.com>
2026-01-16 10:06:20 +0700

    feat: update friend request routes to use POST method and add Friendship schema to API documentation

--- backend/src/routes/customer/friendRoute.js
+++ backend/src/routes/customer/friendRoute.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const friendController = require("../../controllers/customer/friendController");
 
 router.post("/request/:userId", friendController.sendFriendRequest);
-router.put("/accept/:userId", friendController.acceptFriendRequest);
-router.delete("/reject/:userId", friendController.rejectFriendRequest);
+router.post("/accept/:userId", friendController.acceptFriendRequest);
+router.post("/reject/:userId", friendController.rejectFriendRequest);
 router.delete("/unfriend/:userId", friendController.unfriend);
 
 router.get("/list", friendController.getFriends);
